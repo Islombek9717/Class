fe208f3015e04143a486cdf863c6f176aaa27ffd
Islombek <sultonovislom9717@gmail.com>
2022-03-03 23:55:02 +0500

    Add project files.

---
+++ Class/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book (string aTitle, string aAuthor, int Apages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = Apages;
+        }
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+    
+            Book book2 = new Book("Lord Of the Rings", "Tolkein", 700);
+          
+
+            Console.ReadLine();
+        }
+    }
+}
